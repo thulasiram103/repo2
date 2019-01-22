a927f1e1225151afe89991b79659739e31ad607a
thulasiram103 <46807717+thulasiram103@users.noreply.github.com>
2019-01-22 11:48:47 +0530

    Add files via upload

---
+++ level10.php
@@ -0,0 +1,11 @@
+
+<h2>Show my File!</h2>
+<p>
+    A simple utility to get any file via urls
+</p>
+<!-- we have the flag in rctf/flag.php ! But there are certain hurdles to jump over :) -->
+<form method="get">
+    <input name="url" type="text">
+    <button name="getFile" type="submit">Get File</button>
+</form>
+
